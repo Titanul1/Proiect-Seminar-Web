0f0ad160e8cc6f1c7829ac104d55285103c5a9f2
hannachung01 <hannachung01@gmail.com>
2023-01-12 19:06:05 +0200

    front-end ok doar trebuie sa conectezi la backend

--- verificarelitera.js
+++ verificarelitera.js
@@ -16,99 +16,50 @@ function initializare()
     numstelute = cuvant.length;
     numghici = 3;
     document.getElementById("nrghici").innerHTML = numghici
+    document.getElementById("butonv").disabled = false;
 }
 
 function ghicire()
 {
-    if (initializat == true)
+    var litera = document.getElementById("ialitera").value.toUpperCase();
+    if (verifica(litera) == false)
     {
-        if (numghici != 0)
-        {
-            var litera = document.getElementById("ialitera").value.toUpperCase();
-            if (!verifica(litera)) numghici--;
-            document.getElementById("arata").innerHTML =           
-        }
-        else document.getElementById("arata").innerHTML = "Jocul s-a terminat. Initialeaza un joc nou!";
+        numghici--;
+        document.getElementById("nrghici").innerHTML = numghici;
     }
-    else
+    if (numghici == 0)
     {
-        document.getElementById("arata").innerHTML = "Jocul nu este initializat.";    
+        document.getElementById("arata").innerHTML = "Ati pierdut jocul.";
+        document.getElementById("butonv").disabled = true;
     }
-
+    else if (numstelute == 0)
+    {
+        document.getElementById("arata").innerHTML = "Ati castigat jocul!";
+        document.getElementById("butonv").disabled = true;
+    }
+    document.getElementById("ialitera").value = "";
 }
 
-function verifica(ch)
+function verifica(ch) //daca este gasit sau deja folosit, returneaza true
 {
     ch = ch[0];
-    gasit = false;
+    gasit=false;
     for (let i = 0; i < cuvant.length; i++)
     {
-        if (cuvant[i]==modificat[i]) return true;
-
-        if (ch == cuvant[i] && cuvant[i] != modificat[i])
+        if (ch == cuvant[i])
         {
-            stanga = modificat.slice(0, i);
-            dreapta = modificat.slice(i+1);
-            modificat= stanga + ch + dreapta;
-            document.getElementById("arata").innerHTML = modificat;
-            numstelute--;
-            gasit = true;
+            if (cuvant[i]==modificat[i]) return true;
+            else
+            {
+                stanga = modificat.slice(0, i);
+                dreapta = modificat.slice(i+1);
+                modificat= stanga + ch + dreapta;
+                document.getElementById("arata").innerHTML = modificat;
+                numstelute--;
+                gasit=true;
+            }
         }
     }
-    if (gasit == true) return true;
+    if (gasit==true) return true;
     else return false;
 }
-
-
-/*
-function ghicire()
-{
-    if (initializat == true)
-    {
-        if (numghici != 0 && numstelute !=0)
-        { 
-            var litera = document.getElementById("ialitera").value.toUpperCase();
-            if (!cautare(litera))
-            {
-                numghici--;
-                document.getElementById("nrghici").innerHTML = numghici;
-                if (numghici==0)
-                {
-                    if (numstelute !=0) document.getElementById("arata").innerHTML = "Ai pierdut jocul!";
-                    initializat = false;
-                }
-            }
-            else 
-            {
-                if (numstelute ==0) document.getElementById("arata").innerHTML = "Ai castigat jocul!";
-                initializat = false;
-            }
-            document.getElementById("ialitera").value = "";
-        }
-    }
-    else 
-    {
-        document.getElementById("arata").innerHTML = "Jocul nu este initializat.";    
-    }
-}
-
-function cautare(ch)
-{
-    ch = ch[0];
-    gasit = false;
-    for (let i = 0; i < cuvant.length; i++)
-    {
-        if (ch == cuvant[i] && cuvant[i] != modificat[i])
-        {
-            stanga = modificat.slice(0, i);
-            dreapta = modificat.slice(i+1);
-            modificat= stanga + ch + dreapta;
-            document.getElementById("arata").innerHTML = modificat;
-            numstelute--;
-            gasit = true;
-        }
-    }
-    if (gasit == true) return true;
-    else return false;
-}
-*/
